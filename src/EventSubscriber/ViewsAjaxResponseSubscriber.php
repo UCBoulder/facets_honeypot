8172d86d1244350ad4061a43a712faa36de4f73c
protitude <miles.france@gmail.com>
2025-09-09 16:04:18 -0400

    phpcs: class injection

--- src/EventSubscriber/ViewsAjaxResponseSubscriber.php
+++ src/EventSubscriber/ViewsAjaxResponseSubscriber.php
@@ -2,7 +2,9 @@
 
 namespace Drupal\facets_honeypot\EventSubscriber;
 
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
@@ -10,6 +12,33 @@ use Symfony\Component\HttpKernel\KernelEvents;
  * Alter a Views Ajax Response.
  */
 class ViewsAjaxResponseSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The request stack.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
+   * The logger factory.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
+   */
+  protected $loggerFactory;
+
+  /**
+   * Constructs a new ViewsAjaxResponseSubscriber.
+   *
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
+   *   The request stack.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
+   *   The logger factory.
+   */
+  public function __construct(RequestStack $request_stack, LoggerChannelFactoryInterface $logger_factory) {
+    $this->requestStack = $request_stack;
+    $this->loggerFactory = $logger_factory;
+  }
 
   /**
    * {@inheritdoc}
@@ -27,10 +56,11 @@ class ViewsAjaxResponseSubscriber implements EventSubscriberInterface {
    *   The response event.
    */
   public function onResponse(ResponseEvent $event) {
-    $bot = \Drupal::request()->query->get('field_not-an-option-65461');
+    $request = $this->requestStack->getCurrentRequest();
+    $bot = $request->query->get('field_not-an-option-65461');
 
     if ($bot) {
-      \Drupal::logger('facets_honeypot')->debug('Bot box checked');
+      $this->loggerFactory->get('facets_honeypot')->debug('Bot box checked');
     }
 
   }
